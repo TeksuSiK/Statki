d12caea46088ff6288308cbb06d691f66ba4fc9b
TeksuSiK <oskar@teksusik.pl>
2026-05-27 19:27:23 +0200

    Initial commit

---
+++ src/main/java/pl/teksusik/statki/StatkiGame.java
@@ -0,0 +1,4 @@
+package pl.teksusik.statki;
+
+public class StatkiGame {
+}
---
+++ src/test/java/pl/teksusik/statki/StatkiGameTest.java
@@ -0,0 +1,13 @@
+package pl.teksusik.statki;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatkiGameTest {
+
+    @Test
+    void assert_true_test() {
+        assertTrue(true);
+    }
+}
